78b3eef6a11e9d50a98180c8dbd348dfdb18e010
Matthew Jorgensen <matthew@jrgnsn.net>
2019-10-15 20:34:24 -0500

    Show the albums route on / for now

--- server.go
+++ server.go
@@ -32,7 +32,7 @@ func (s *Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 func (s *Server) routes() {
 	r := s.router
-	r.HandleFunc("/", s.homeHandler)
+	r.HandleFunc("/", s.handleAlbumIndex)
 	r.HandleFunc("/login", s.loginHandler).Methods("GET", "POST")
 
 	r.HandleFunc("/album", s.handleGetAlbum)
