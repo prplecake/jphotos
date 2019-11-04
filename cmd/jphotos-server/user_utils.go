93e55f1b68e4de24f71d179203a0e40b84099ee8
Matthew Jorgensen <matthew@jrgnsn.net>
2019-11-04 16:30:00 -0600

    Fix .gitignore and add files to actually implement manage users from CLI

---
+++ cmd/jphotos-server/user_utils.go
@@ -0,0 +1,55 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"os"
+	"strings"
+	"syscall"
+
+	"golang.org/x/crypto/ssh/terminal"
+
+	"git.sr.ht/~mjorgensen/jphotos/auth"
+	"git.sr.ht/~mjorgensen/jphotos/db"
+)
+
+func createUser(dbs *db.PGStore) {
+	username, password := credentials()
+
+	err := auth.AddUser(username, password, dbs)
+	if err != nil {
+		log.Fatal("An error occured attempting to add user:", err)
+	}
+
+	log.Printf("User '%s' created.", username)
+}
+
+func deleteUser(username string, dbs *db.PGStore) {
+	if len(username) == 0 {
+		reader := bufio.NewReader(os.Stdin)
+
+		fmt.Print("Enter Username: ")
+		username, _ = reader.ReadString('\n')
+		username = strings.TrimSpace(username)
+	}
+
+	err := auth.RemoveUser(username, dbs)
+	if err != nil {
+		log.Fatalf("An error occured attempting to remove user [%s]: %v", username, err)
+	}
+	log.Printf("User '%s' deleted.", username)
+}
+
+func credentials() (string, string) {
+	reader := bufio.NewReader(os.Stdin)
+
+	fmt.Print("Enter Username: ")
+	username, _ := reader.ReadString('\n')
+
+	fmt.Print("Enter Password: ")
+	bytePassword, _ := terminal.ReadPassword(int(syscall.Stdin))
+	password := string(bytePassword)
+
+	return strings.TrimSpace(username), strings.TrimSpace(password)
+}
